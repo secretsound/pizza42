712735d6e1130b65e427145af5be748fe25ddd13
Luke Blankenship <lukeblankenship@gmail.com>
2022-03-13 20:24:45 -0700

    Added check for proper redirect

--- src/boot/auth0.js
+++ src/boot/auth0.js
@@ -4,10 +4,17 @@ import { boot } from 'quasar/wrappers';
 import { createAuth0 } from '@auth0/auth0-vue';
 import { api_audience, auth0_domain } from './auth0_conf';
 
+let redirect_uri;
+if (process.env.NODE_ENV === 'production') {
+  redirect_uri = 'https://pizza-42-2-pddo4.ondigitalocean.app/#/';
+} else {
+  redirect_uri = 'https://localhost:8080/#/';
+}
+
 export const auth0Vue = createAuth0({
   domain: auth0_domain,
   client_id: 'IaN1bDUvMr22u6SSFPNJ28XDpApLqB3K',
-  redirect_uri: 'http://localhost:8080/#/',
+  redirect_uri: redirect_uri,
   audience: api_audience,
   scope: 'write:order',
   cacheLocation: 'localstorage',
